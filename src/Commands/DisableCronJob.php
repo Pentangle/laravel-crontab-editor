121c1477c241ec1db75c7026e02ec65021753d32
Séan Poynter-Smith <spoyntersmith@users.noreply.github.com>
2021-07-08 18:24:53 +0100

    add cron disable cmd
    Signed-off-by: Séan Poynter-Smith <spoyntersmith@users.noreply.github.com>

---
+++ src/Commands/DisableCronJob.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Pentangle\LaravelCrontabEditor\Commands;
+
+use Illuminate\Console\Command;
+use TiBeN\CrontabManager\CrontabAdapter;
+use TiBeN\CrontabManager\CrontabJob;
+use TiBeN\CrontabManager\CrontabRepository;
+
+class DisableCronJob extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'crontab-editor:disable';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $crontabRepository = new CrontabRepository(new CrontabAdapter());
+        $path = getcwd();
+        $crontabJob = CrontabJob::createFromCrontabLine('* * * * * cd '.$path.' && php artisan schedule:run >> /dev/null 2>&1');
+        $crontabRepository->removeJob($crontabJob);
+        $crontabRepository->persist();
+    }
+}
